e791a52823e45f64f8bf6775f3cd426fefe79167
zybe <13022014082@163.com>
2021-06-03 18:22:48 +0800

    update database setting

--- init.sql
+++ init.sql
@@ -3,35 +3,39 @@ CREATE DATABASE `building`;
 USE `building`;
 
 CREATE TABLE `lift`(
+	`id` INT AUTO_INCREMENT,
 	`device_sn` NVARCHAR(10),
-	`localetime` NVARCHAR(10),
-	`weight` DECIMAL(10,3),
-	`height` DECIMAL(10,3),
-	`speed` DECIMAL(10,3),
-	`tilt` DECIMAL(10,3),
+	`localetime` NVARCHAR(20),
+	`weight` DECIMAL(10,6),
+	`height` DECIMAL(10,6),
+	`speed` DECIMAL(10,6),
+	`tilt` DECIMAL(10,6),
 	`direction` INT,
-	`rated_weight` DECIMAL(10,3),
-	`rated_height` DECIMAL(10,3),
-	`rated_speed` DECIMAL(10,3),
+	`rated_weight` DECIMAL(10,6),
+	`rated_height` DECIMAL(10,6),
+	`rated_speed` DECIMAL(10,6),
 	`door` INT,
 	`state` INT,
-	`windspeed` DECIMAL(10,3)
+	`windspeed` DECIMAL(10,6),
+	PRIMARY KEY(`id`)
 	);
 
 	CREATE TABLE `tower`(
+	`id` INT AUTO_INCREMENT,
 	`device_sn` NVARCHAR(10),
-	`localetime` NVARCHAR(10),
-	`height` DECIMAL(10,3),
-	`weight` DECIMAL(10,3),
-	`scope` DECIMAL(10,3),
-	`rotate` DECIMAL(10,3),
-	`windSpeed` DECIMAL(10,3),
-	`oblique` DECIMAL(10,3),
-	`torque` DECIMAL(10,3),
-	`cargo_boom` DECIMAL(10,3),
-	`balance_arm` DECIMAL(10,3),
-	`rated_weight` DECIMAL(10,3),
-	`rated_height` DECIMAL(10,3),
-	`rated_torque` DECIMAL(10,3),
-	`state` INT
+	`localetime` NVARCHAR(20),
+	`height` DECIMAL(10,6),
+	`weight` DECIMAL(10,6),
+	`scope` DECIMAL(10,6),
+	`rotate` DECIMAL(10,6),
+	`windSpeed` DECIMAL(10,6),
+	`oblique` DECIMAL(10,6),
+	`torque` DECIMAL(10,6),
+	`cargo_boom` DECIMAL(10,6),
+	`balance_arm` DECIMAL(10,6),
+	`rated_weight` DECIMAL(10,6),
+	`rated_height` DECIMAL(10,6),
+	`rated_torque` DECIMAL(10,6),
+	`state` INT,
+	PRIMARY KEY(`id`)
 	);
